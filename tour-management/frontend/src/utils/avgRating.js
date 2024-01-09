25c598e8a1ae9ec1ff245a88417b4513dde48f4a
KANWAL SHEHZADI <137278493+KanwalShehzadi98-ux@users.noreply.github.com>
2024-01-09 22:19:11 +0500

    Add files via upload

---
+++ tour-management/frontend/src/utils/avgRating.js
@@ -0,0 +1,42 @@
+
+
+// const calculateAvgRating = reviews => {
+//     const totalRating = reviews.reduce((acc,item)=>acc+item.rating,0)
+//     const avgRating = 
+//     totalRating == 0 
+//     ? ''
+//     : totalRating == 1 
+//     ? totalRating
+//     :(totalRating / reviews?.length).toFixed(1);
+
+//     return {
+//         totalRating,
+//         avgRating
+//     }
+// }
+
+// export default calculateAvgRating;
+
+const calculateAvgRating = reviews => {
+    if (!reviews || !Array.isArray(reviews) || reviews.length === 0) {
+        return {
+            totalRating: 0,
+            avgRating: ''
+        };
+    }
+
+    const totalRating = reviews.reduce((acc, item) => acc + item.rating, 0);
+    const avgRating =
+        totalRating === 0
+            ? ''
+            : totalRating === 1
+            ? totalRating
+            : (totalRating / reviews.length).toFixed(1);
+
+    return {
+        totalRating,
+        avgRating
+    };
+};
+
+export default calculateAvgRating;
